d47f51b9f71b9793bbdac0685a20fffdb04a6092
Pablo <ollbap@localhost.localdomain>
2018-06-02 17:50:35 +0200

    First commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+mvn install dockerfile:build
---
+++ debug.sh
@@ -0,0 +1,1 @@
+docker run -e "JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,address=5005,server=y,suspend=n" -p 8080:8080 -p 5005:5005 -t pablo/my-spring-boot-project
